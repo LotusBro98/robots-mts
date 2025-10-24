13174991274c78dd4ce64b15374a31f79d43a390
Lotus Bro <lotusbro98@gmail.com>
2025-10-24 19:06:57 +0100

    chassis WIP

--- chassis.py
+++ chassis.py
@@ -1,9 +1,12 @@
 import json
+import traceback
 import serial
+import threading
 
 class RobotChassis:
     def __init__(self, port: str = "/dev/ttyACM1"):
         self.port = port
+        self.sensors_thread = threading.Thread
         
     def connect(self):
         self.ser = serial.Serial(self.port, baudrate=115200, timeout=1)
@@ -21,13 +24,24 @@ class RobotChassis:
         # response = json.loads(response)
         # return response
 
-    def capture_wheel_sensors(self):
+    def _capture_wheel_sensors(self):
         while True:
-            msg = self.ser.read_until(b"\r\n").strip(b"\r\n")
+            try:
+                msg = self.ser.read_until(b"\r\n").strip(b"\r\n")
+            except:
+                traceback.print_exc()
+                break
+
+            try:
+                msg = json.loads(msg.decode())
+            except:
+                traceback.print_exc()
             print(msg) # b'{"T":1001,"M1":0,"M2":0,"M3":0,"M4":0,"odl":3247,"odr":8920,"v":963}'
         # res = self.send_command(T=130)
         print(res)
 
+    def sensors_callback(self, msg):
+        print("Chassis sensors: ", msg)
 
 def main():
     robot = RobotChassis("/dev/ttyACM1")
