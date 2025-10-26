d8ab156074d7df386d9bc7b3bcdc367d96dbc061
Lotusbro98 <lotusbro98@gmail.com>
2025-10-26 16:45:59 +0300

    test drive

---
+++ test_drive.py
@@ -0,0 +1,54 @@
+import time
+
+from chassis import RobotChassis
+from driver import Driver
+
+robot = RobotChassis()
+robot.connect()
+driver = Driver(robot)
+driver.update_ref_angle()
+
+# повернуться на определённый градус
+print("Rotate 90")
+driver.rotate(90)
+driver.freeze()
+print("done")
+
+# проехать заданное расстояние
+print("Drive 0.5 m")
+driver.drive(max_speed=0.5, max_dist=0.5)
+driver.freeze()
+print("Done")
+
+# подъехать к стенке (не ближе заданного расстояния)
+print("Drive until 0.5 m to wall")
+driver.drive(max_speed=0.5, front_wall_dist=0.5)
+driver.freeze()
+print("Done")
+
+# поездить вдоль стенки
+print("Drive right wall 2 m")
+driver.drive(right_wall_dist=0.5, max_dist=2, max_speed=0.5)
+driver.freeze()
+print("Done")
+
+
+# print("connect_robot")
+# robot.connect()
+# robot.start_sensor_capture()
+# print("robot connected")
+# time.sleep(5)
+# robot.send_drive(v=1, w=0)
+# print("Оба мотора едут прямо")
+# time.sleep(10)
+# robot.send_drive(v=0, w=0)
+# print("Оба остановлены")
+# time.sleep(5)
+# robot.send_drive(v=0.5, w=1)
+# print("Широкий поворот налево")
+# time.sleep(10)
+# robot.send_drive(v=0, w=0)
+# print("Оба остановлены")
+# time.sleep(5)
+# robot.disconnect()
+# print("disconnect_robot")
