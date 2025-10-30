4cd817ba305f8d8279579714257a6c1b3c4f5fdd
Lotusbro98 <lotusbro98@gmail.com>
2025-10-30 17:16:49 +0300

    default plan

--- drive_planner.py
+++ drive_planner.py
@@ -7,6 +7,23 @@ class RobotDrivePlanner:
         self.driver = driver
         self.navigator = driver.robot.navigator
         self.robot = driver.robot
+
+        self.DRIVES = [
+            (self.driver.maze_forward, (0.25,)),
+            (self.driver.maze_turn, (90,)),
+            (self.driver.maze_turn, (-90,)),
+            (self.driver.maze_turn, (90,)),
+            (self.driver.maze_turn, (-90,)),
+            (self.driver.maze_forward, (0.5,)),
+            (self.driver.maze_turn, (-90,)),
+            (self.driver.maze_forward, (0.5,)),
+            (self.driver.maze_turn, (90,)),
+            (self.driver.maze_forward, (3,)),
+            (self.driver.maze_turnaround, ()),
+            (self.driver.maze_forward, (3,)),
+        ]
+        
+
     """
     использует Driver для вызова манёвров, Robot.recv_sensors для получения данных датчиков, Robot.navigator.
     Главная логика:
@@ -15,13 +32,21 @@ class RobotDrivePlanner:
     - если впереди коридор, едем до поворота или на макс расстояние пока не станет видно дальше
     - если сейчас есть поворот налево -> налево
     """
+    cnt = 0
     def calculate_next_drive(self):
         """
         Рассчитывает следующий манёвр для текущего положения робота.
-        Наверное другой метод будет в цикле вызывать этот, и отправлять его на моторы.
         """
-        args = ()
-        return (self.driver.maze_forward, (args))
+        ret = self.DRIVES[self.cnt] + ({},)
+        self.cnt += 1
+        return ret
+    
+    def main_loop(self):
+        while True:
+            next_drive, args, kwargs = self.calculate_next_drive()
+            next_drive(*args, **kwargs)
+            if self.cnt >= len(self.DRIVES):
+                break
 
     @staticmethod
     def _calculate_distance_to_next_turn_in_corridor(points: np.ndarray) -> tuple[str, float]:
--- maze_main.py
+++ maze_main.py
@@ -1,19 +1,27 @@
+from drive_planner import RobotDrivePlanner
 from driver import Driver
 from emulation.emulated_robot import EmulatedRobot
 
 robot = EmulatedRobot(demo_render_navigator=False, file_rendering_navigator=True)  # Flags: demo_render_navigator, file_rendering_navigator
 robot.connect()
 driver = Driver(robot)
+planner = RobotDrivePlanner(driver)
 
-driver.maze_forward(0.25)
-driver.maze_turn(90)
-driver.maze_turn(-90)
-driver.maze_turn(90)
-driver.maze_turn(-90)
-driver.maze_forward(0.5)
-driver.maze_turn(-90)
-driver.maze_forward(0.5)
-driver.maze_turn(90)
-driver.maze_forward(3)
-driver.maze_turnaround()
-driver.maze_forward(3)
+try:
+    planner.main_loop()
+finally:
+    driver.stop()
+    robot.disconnect()
+
+# driver.maze_forward(0.25)
+# driver.maze_turn(90)
+# driver.maze_turn(-90)
+# driver.maze_turn(90)
+# driver.maze_turn(-90)
+# driver.maze_forward(0.5)
+# driver.maze_turn(-90)
+# driver.maze_forward(0.5)
+# driver.maze_turn(90)
+# driver.maze_forward(3)
+# driver.maze_turnaround()
+# driver.maze_forward(3)
